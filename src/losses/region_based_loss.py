087086274d5f621a33b3886de1bcce8fd5525b6c
fatima623 <fatima.bscs238@iiu.edu.pk>
2024-09-12 10:01:19 +0500

    def forward removed

--- src/losses/region_based_loss.py
+++ src/losses/region_based_loss.py
@@ -17,20 +17,3 @@ class RegionBasedDiceLoss3D(nn.Module):
                                                eval_regions=True)
 
 
-    def forward(self, input: torch.tensor, target: torch.tensor, weight_reg: int=1, weight_dice: int=1):
-        """
-        Weights for CE and Dice do not need to sum to one. You can set whatever you want.
-
-        Forward pass
-
-        :param input: torch.tensor (NxCxDxHxW) Network output
-        :param target: ground truth torch.tensor (NxDxHxW)
-        :param weight: torch.tensor (N) to provide class weights
-        :return: scalar
-       """
-        dice_loss, dice_score, _ = self.dice_loss(input, target)
-        dice_loss_reg, _, subregions = self.dice_loss_region_based(input, target)
-
-        total_loss = weight_dice*dice_loss + weight_reg*dice_loss_reg
-
-        return total_loss, dice_loss, dice_score, dice_loss_reg, subregions
